701b8aac4660ee24c4a6797abd265c8788c008b5
lythanhlongdev <lythanhlongfmc@gmail.com>
2023-11-24 00:06:01 +0700

    Update

--- BE-DNSOS/src/main/java/com/capstone2/dnsos/dto/history/HistoryDTO.java
+++ BE-DNSOS/src/main/java/com/capstone2/dnsos/dto/history/HistoryDTO.java
@@ -1,20 +1,10 @@
 package com.capstone2.dnsos.dto.history;
 
 
-import com.capstone2.dnsos.enums.Status;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
-
-import java.awt.*;
-import java.io.File;
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -30,7 +20,7 @@ public class HistoryDTO {
     private Double latitude;// vi do
     @NotNull(message = "longitude id is requirement")
     private Double longitude;// kinh do
-    @Size(min = 0, max =  200, message = "Note: Enter a maximum of 200 characters")
+    @Size( max =  200, message = "Note: Enter a maximum of 200 characters")
     private String note;
 
 }
