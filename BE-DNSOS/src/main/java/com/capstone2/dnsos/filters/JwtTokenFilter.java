9f6385e87e394318745a5cd69b61c72281e3ac2e
ltldev <lythanhlong.fmc@gmail.com>
2024-03-01 15:12:53 +0700

    update-test

--- BE-DNSOS/src/main/java/com/capstone2/dnsos/filters/JwtTokenFilter.java
+++ BE-DNSOS/src/main/java/com/capstone2/dnsos/filters/JwtTokenFilter.java
@@ -90,8 +90,8 @@ public class JwtTokenFilter extends OncePerRequestFilter {
                 Pair.of(String.format("%s/users/login", apiPrefix), "POST"),
                 Pair.of(String.format("%s/rescue_stations/login", apiPrefix), "POST"),
                 Pair.of(String.format("%s/rescue_stations/register", apiPrefix), "POST"),
-                Pair.of(String.format("%s/users/register", apiPrefix), "POST"),
-                Pair.of(String.format("%s/address/**", apiPrefix), "GET")
+                Pair.of(String.format("%s/users/register", apiPrefix), "POST")
+//                Pair.of(String.format("%s/address/**", apiPrefix), "GET")
 
                 // Swagger
 //                Pair.of("/api-docs","GET"),
